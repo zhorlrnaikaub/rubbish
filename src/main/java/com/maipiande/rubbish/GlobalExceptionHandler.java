f3964c867e4a1f07f1cfba2b92030b2f3f07228f
zhorlrnaikaub <tangyuanca@163.com>
2021-04-21 22:48:39 +0800

    增加异常处理

--- src/main/java/com/maipiande/rubbish/GlobalExceptionHandler.java
+++ src/main/java/com/maipiande/rubbish/GlobalExceptionHandler.java
@@ -2,13 +2,24 @@ package com.maipiande.rubbish;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.converter.HttpMessageConversionException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/**
+ * 异常处理
+ */
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(RuntimeException.class)
     public RubbishResult handleRuntimeException(RuntimeException e) {
+
+        if (e instanceof HttpMessageConversionException){
+            log.error("请求参数异常："+e.getMessage(),e);
+            return RubbishResult.fail("参数异常");
+        }
         log.error("rubbis exception is ....",e.getMessage(),e);
         return RubbishResult.fail(e.getMessage());
     }
