74fe7e3b4c5ed380b144a61828b949d24ee66014
zhorlrnaikaub <tangyuanca@163.com>
2021-04-21 23:06:33 +0800

    文章列表SQL错误修复

--- src/test/java/com/maipiande/rubbish/RubbishApplicationTests.java
+++ src/test/java/com/maipiande/rubbish/RubbishApplicationTests.java
@@ -1,16 +1,31 @@
 package com.maipiande.rubbish;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RubbishApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
-	public void contextLoads() {
+	public void articleList() {
+		Map<String,String> param = new LinkedHashMap<>();
+		param.put("title","111");param.put("classify","111");
+		ResponseEntity<RubbishResult> response = restTemplate.postForEntity("/article/list", param, RubbishResult.class);
+		RubbishResult result = response.getBody();
+		Assert.assertEquals(result.getStatus(),0);
 	}
 
 }
